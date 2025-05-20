25a4fd9dea130556f01267a0b31354f7e15f3a47
Rudranshpardeshi09 <rudranshpardeshi12@gmail.com>
2025-05-20 17:02:12 +0530

    Added Frontend Authentication

--- Frontend-Certificate/src/App.jsx
+++ Frontend-Certificate/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import RegisterForm from './components/Login/text.jsx'
 import './App.css'
 import Login from './components/Login/Login'
 
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div>
-        <Login></Login>
+        {/* <RegisterForm/> */}
+        <Login/>
       </div>
     </>
   )
