ef1bc72f9c6146104ac46ca9c12d16e5492e3307
Ashwin <Ashwinchouhan567@gmail.com>
2025-06-06 13:25:12 +0530

    Home Page added

--- Frontend-Certificate/src/App.jsx
+++ Frontend-Certificate/src/App.jsx
@@ -4,18 +4,20 @@ import './App.css'
 import Login from './components/Login/Login'
 import Upload from './components/upload/upload'
 import Editor from './components/Editor/Editor'
+import Header from './components/home/header'
 
 function App() {
   //const [count, setCount] = useState(0)
   const token=localStorage.getItem('token');
   return (
     <>
-      <Routes>
+      <Header></Header>
+      {/* <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/upload" element={<Upload />} />
         {/* {token && <Route path="/upload" element={<Upload />}/>} */}
-        <Route path="/editor" element={<Editor/>}/>
-      </Routes>
+        {/* <Route path="/editor" element={<Editor/>}/>
+      </Routes> */}
     </>
   )
 }
