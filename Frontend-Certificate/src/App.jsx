6e0137bd0ff5476f0a821d602d5aaa8f2d4f0612
Shubhansh Sharma <123613015+ShubhanshSharma@users.noreply.github.com>
2025-06-10 13:22:43 +0530

    Update App.jsx to fix build logs

--- Frontend-Certificate/src/App.jsx
+++ Frontend-Certificate/src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 import Login from './components/Login/Login'
 import Upload from './components/upload/upload'
 import Editor from './components/Editor/Editor'
-// import Header from './components/home/header'
 import { AdminLayout } from './components/Admin pages/Admin'
 import Dashboard from './components/Admin pages/Dashboard/adminDashboard'
 import ChangePassword from './components/Admin pages/Change Password/changePassword'
@@ -25,7 +24,6 @@ function App() {
     
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path='/header' element={<Header/>}/> */}
         {/* <Route path="/upload" element={<Upload />} /> */}
         <Route path="/loader" element={<Loader />} />
         {token && <Route path="/upload" element={<Upload />}/>}
