f5792d54bda33ae5d034a53dbc460bf79b74ef6c
Shubhansh Sharma <shubhanshsharma030604@gmail.com>
2025-06-13 17:27:51 +0530

    changes for backend deployment

--- certificate-backend/backend/settings.py
+++ certificate-backend/backend/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-rx_qyy5wzq&37p5_q+-wn!1qvxl%e@my53i*yz-$m&07y@!oli
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,14 +128,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'certificate-local',       
+#         'USER': 'postgres',      
+#         'PASSWORD': 1234,
+#         'HOST': 'localhost',
+#         'PORT': '1910',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'certificate-local',       
-        'USER': 'postgres',      
-        'PASSWORD': 1234,
-        'HOST': 'localhost',
-        'PORT': '1910',
+        'NAME': os.getenv('DB_NAME'),       
+        'USER': os.getenv('DB_USER') ,      
+        'PASSWORD': os.getenv('DB_PASSWORD') ,
+        'HOST': os.getenv('DB_HOST') ,
+        'PORT': os.getenv('DB_PORT') ,
     }
 }
 
@@ -182,6 +194,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
