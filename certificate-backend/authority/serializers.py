f0ed47cea09b40ba091ee3f5eb9b75f806f7fdbe
roshniverma05 <163026505+roshniverma05@users.noreply.github.com>
2025-07-09 13:54:04 +0530

    upgraded

--- certificate-backend/authority/serializers.py
+++ certificate-backend/authority/serializers.py
@@ -82,7 +82,9 @@
 #         def create(self, validated_data):
 #             password = validated_data.pop('password')
 #             user = CustomUser(**validated_data)
-#             user.set_password(password)
+#       
+# 
+#       user.set_password(password)
 #             user.is_staff = True
 #             user.save()
 #             return user
